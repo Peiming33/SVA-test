2da462c7b462374a6228031f926c96497e4737eb
Peiming33 <phao@sva.edu>
2025-01-30 19:29:45 -0500

    css practice

---
+++ code/week2/sketch.js
@@ -0,0 +1,25 @@
+let btn
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  rectMode(CENTER)
+  btn = select('#my-button')
+  btn.mouseClicked(onBtnClicked)
+
+}
+
+function onBtnClicked(){
+  fill(random(225));
+}
+function draw() {
+
+  if (mouseIsPressed){
+    line(pmouseX, pmouseY, mouseX, mouseY)
+  }
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight)
+}
---
+++ code/week3/02-webpage/sketch.js
@@ -0,0 +1,8 @@
+function setup() {
+    createCanvas(400, 400);
+    background(190)
+    ellipse(100,100,100)
+  }
+  
+  function draw() {
+  }
