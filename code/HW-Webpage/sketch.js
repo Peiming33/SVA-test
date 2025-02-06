5ee604d0b4079fd7aa09dfc01b78f8230c697b92
Peiming33 <phao@sva.edu>
2025-02-05 21:05:52 -0500

    Complete rest of the webpage

--- code/HW-Webpage/sketch.js
+++ code/HW-Webpage/sketch.js
@@ -2,6 +2,10 @@ let text = document.getElementById('text');
 let building = document.getElementById('building');
 let butterfly1 = document.getElementById('butterfly1');
 let white = document.getElementById('white');
+let Green = document.getElementById('Green');
+let SpaceFor = document.getElementById('SpaceFor');
+let Everyone = document.getElementById('Everyone');
+let Picture2 = document.getElementById('Picture2');
 
 window.addEventListener('scroll', () =>{
   let value = window.scrollY;
@@ -9,4 +13,14 @@ window.addEventListener('scroll', () =>{
   text.style.marginTop = value * 2.5 + 'px';
   building.style.left = value * 1.5 + 'px';
   butterfly1.style.left = value * -1.5 + 'px';
+  Picture2.style.left = value * 0.035 + 'px';
+
+  if(window.scrollY>350){
+    text.classList.add("hidden");
+  }else{
+    text.classList.remove("hidden");
+  }
+
 })
+
+
